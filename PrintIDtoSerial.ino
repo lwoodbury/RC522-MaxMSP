0de5e9ba67cf196b2db477e2e98421db867ee8f6
Luke Woodbury <luke@lukewoodbury.co.uk>
2016-02-25 09:26:22 +0000

    First commit
    Print tag ID to serial
    Format and store in Max MSP patch

---
+++ PrintIDtoSerial.ino
@@ -0,0 +1,56 @@
+/*
+ * --------------------------------------------------------------------------------------------------------------------
+ * Print the RFID tag ID to serial
+ * --------------------------------------------------------------------------------------------------------------------
+ * This sketch borrows from the MFRC522 library examples; 
+ * for further details and other examples see: https://github.com/miguelbalboa/rfid
+ *
+ * Typical pin layout used:
+ * -----------------------------------------------------------------------------------------
+ *             MFRC522      Arduino       Arduino   Arduino    Arduino          Arduino
+ *             Reader/PCD   Uno           Mega      Nano v3    Leonardo/Micro   Pro Micro
+ * Signal      Pin          Pin           Pin       Pin        Pin              Pin
+ * -----------------------------------------------------------------------------------------
+ * RST/Reset   RST          9             5         D9         RESET/ICSP-5     RST
+ * SPI SS      SDA(SS)      10            53        D10        10               10
+ * SPI MOSI    MOSI         11 / ICSP-4   51        D11        ICSP-4           16
+ * SPI MISO    MISO         12 / ICSP-1   50        D12        ICSP-1           14
+ * SPI SCK     SCK          13 / ICSP-3   52        D13        ICSP-3           15
+ * 3V to 3V and GND to GND!!!
+ * 
+ * Luke Woodbury 24th February 2016
+ */
+
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define RST_PIN   9     // Configurable, see typical pin layout above
+#define SS_PIN    10    // Configurable, see typical pin layout above
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance
+
+void setup() {
+  Serial.begin(9600);  // Initialize serial communications with the PC
+  while (!Serial);     // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
+  SPI.begin();         // Init SPI bus
+  mfrc522.PCD_Init();  // Init MFRC522 card
+ }
+
+void loop() {
+  
+  // Look for new cards, and select one if present
+  if ( ! mfrc522.PICC_IsNewCardPresent() || ! mfrc522.PICC_ReadCardSerial() ) {
+    delay(50);
+    return;
+  }
+  
+  // Now a card is selected. The UID and SAK is in mfrc522.uid.
+  // Dump UID
+  //Serial.print(F("Card UID:"));
+  for (byte i = 0; i < mfrc522.uid.size; i++) {
+    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+    Serial.print(mfrc522.uid.uidByte[i], HEX);
+  } 
+  Serial.println();
+
+}
